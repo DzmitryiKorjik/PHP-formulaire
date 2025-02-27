2fe3ec8637f646d025bc2875ce3ac2a6c77b16f4
DzmitryiKorjik <dmardovitch@gmail.com>
2025-02-27 10:39:18 +0100

    .env

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php'; // Подключаем Composer
+require_once __DIR__ . '/vendor/autoload.php'; // Connexion Composer
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__); // Загружаем .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__); // Chargement .env
 $dotenv->load();
 
-// Проверяем, загружены ли переменные
+// Vérifier si les variables sont chargées
 if (!isset($_SERVER['DB_HOST']) || !isset($_SERVER['DB_NAME']) || 
     !isset($_SERVER['DB_USER']) || !isset($_SERVER['DB_PASSWORD'])) {
     die('Erreur : impossible de charger les variables .env');
 }
 
-// Используем $_SERVER
+// Utilise $_SERVER
 $db_host = $_SERVER['DB_HOST'];
 $db_name = $_SERVER['DB_NAME'];
 $db_user = $_SERVER['DB_USER'];
@@ -20,6 +20,6 @@ try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
+    echo "Erreur de connexion : " . $e->getMessage();
 }
 ?>
--- create_user.php
+++ create_user.php
@@ -1,16 +1,5 @@
 <?php
-    $host = 'mysql';
-    $dbname = 'user_connexion';
-    $username = 'root';
-    $password = 'root';
-    
-    try {
-        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-        // Définit le mode d'erreur PDO sur exception
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch(PDOException $e) {
-        echo "Erreur de connexion : " . $e->getMessage();
-    }
+    require_once 'config.php';
 
     $nom = $_POST['last_name'] ?? null;
     $prenom = $_POST['first_name'] ?? null;
